b9e70ec0ffc7c3da985db4364d24487d7d3053fa
Andre <cavallari@live.com>
2025-06-07 16:14:42 -0300

    chore: Create books resources with index and create

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -4,8 +4,25 @@ module Api
   module V1
     class BooksController < ApplicationController
       def index
-        render json: { message: 'Welcome to the Books API' }, status: :ok
+        books = Book.accessible_by(current_ability)
+        render json: books, status: :ok
       end
+
+      def create
+        authorize! :create, Book
+        book = Book.new(book_params)
+
+        if book.save
+          render json: book, status: :created
+        else
+          render json: { errors: book.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+        def book_params
+          params.require(:book).permit(:title, :author, :genre, :isbn, :copies)
+        end
     end
   end
 end
--- spec/requests/api/v1/books_spec.rb
+++ spec/requests/api/v1/books_spec.rb
@@ -2,6 +2,96 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Books", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    context 'when user is not logged in' do
+      it 'returns an unauthorized status' do
+        get api_v1_books_path
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when user is a member' do
+      let(:member) { create(:user, role: :member) }
+      let(:token) { JsonWebToken.encode(id: member.id) }
+
+      before do
+        create_list(:book, 3)
+        create_list(:book, 2, :unavailable)
+        get api_v1_books_path, headers: { 'Authorization': "Bearer #{token}" }
+      end
+
+      it 'returns a list of available books' do
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body).size).to eq(3)
+      end
+    end
+
+    context 'when user is a librarian' do
+      let(:librarian) { create(:user, role: :librarian) }
+      let(:token) { JsonWebToken.encode(id: librarian.id) }
+
+      before do
+        create_list(:book, 5)
+        create_list(:book, 2, :unavailable)
+        get api_v1_books_path, headers: { 'Authorization': "Bearer #{token}" }
+      end
+
+      it 'returns a list of all books' do
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body).size).to eq(7)
+      end
+    end
+  end
+
+  describe "POST /create" do
+    subject(:action) { post api_v1_books_path, params: valid_attributes }
+    let(:member) { create(:user, role: :member) }
+    let(:librarian) { create(:user, role: :librarian) }
+    let(:json_response) { JSON.parse(response.body) }
+
+    let(:valid_attributes) do
+      {
+        book: {
+          title: "Sample Book",
+          author: "Sample Author",
+          genre: "Fiction",
+          isbn: "1234567890",
+          copies: 3
+        }
+      }
+    end
+
+    context 'when user is not logged in' do
+      it 'returns an unauthorized status' do
+        expect { action }.not_to change(Book, :count)
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when user is a member' do
+      subject(:action) { post api_v1_books_path, params: valid_attributes, headers: {
+        'Authorization': "Bearer #{token}"
+      }}
+
+      let(:token) { JsonWebToken.encode(id: member.id) }
+
+      it 'returns a forbidden status' do
+        expect { action }.not_to change(Book, :count)
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+
+    context 'when user is a librarian' do
+      subject(:action) { post api_v1_books_path, params: valid_attributes, headers: {
+        'Authorization': "Bearer #{token}"
+      }}
+
+      let(:token) { JsonWebToken.encode(id: librarian.id) }
+
+      it 'creates a new book' do
+        expect { action }.to change(Book, :count).by(1)
+        expect(response).to have_http_status(:created)
+        expect(json_response['title']).to eq(valid_attributes[:book][:title])
+      end
+    end
   end
 end
