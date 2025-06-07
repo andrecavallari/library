6c88e3ccff73e5367cffb3226baed4ccd9fe46be
Andre <cavallari@live.com>
2025-06-07 16:35:39 -0300

    chore: Create book show endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :books, only: %i[index create]
+      resources :books, only: %i[index create show]
       resources :auth, only: %i[create destroy]
       resources :users, only: [:create]
     end
