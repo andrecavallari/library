879762f6d3aabfcca5ee611c4d215e3af57bcd0b
Andre <cavallari@live.com>
2025-06-07 10:26:58 -0300

    chore: Add dotenv to app

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
-require_relative "boot"
+# frozen_string_literal: true
 
+require "dotenv/load"
+require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
