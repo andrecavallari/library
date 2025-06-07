7b317a9f71c4935a6cb5a55dc1929dad43ae00b6
Andre <cavallari@live.com>
2025-06-07 13:39:27 -0300

    Create jwt lib

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -3,11 +3,6 @@
 class JsonWebToken
   SECRET_KEY = ENV.fetch('JWT_SECRET_KEY', 'development_secret_key')
 
-  def self.encode(payload)
-    JWT.encode(payload.merge(exp: 1.hour.from_now.to_i), SECRET_KEY)
-  end
-
-  def self.decode(token)
-    JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })
-  end
+  def self.encode(payload) = JWT.encode(payload, SECRET_KEY)
+  def self.decode(token) = JWT.decode(token, SECRET_KEY)
 end
